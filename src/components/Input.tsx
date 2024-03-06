a519ed0c871a7878c29d8e03932f22e16db87176
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-03-06 13:20:42 -0300

    feat: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,16 @@
+import { InputHTMLAttributes } from 'react'
+import { Search } from 'lucide-react'
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>
+
+export function Input({ ...rest }: InputProps) {
+  return (
+    <div className="flex items-center p-3 rounded-lg bg-zinc-600">
+      <Search size={20} className="text-zinc-400" />
+      <input
+        {...rest}
+        className="bg-transparent max-w-80 h-6 text-zinc-100 ml-4 outline-none"
+      />
+    </div>
+  )
+}
