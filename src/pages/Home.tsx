1ad217257b96c48e3444a36812edbb0870f4ea68
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-03-06 19:26:24 -0300

    feat: search feature and export csv data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,86 @@
 import { Input } from '../components/Input'
 import BgHero from '../assets/hero-image-wr.jpg'
-import Logo from '../assets/Logo.svg'
 import { HeaderTable } from '../components/Table/HeaderTable'
 import { ContentTable } from '../components/Table/ContentTable'
 import { OrganizerWrapper } from '../components/Organizer/OrganizerWrapper'
 import { OrganizerSelect } from '../components/Organizer/OrganizerSelect'
-import { CountriesData, useCountry } from '../context/countryContext'
-import { useState } from 'react'
+import { useCountry } from '../context/countryContext'
+import { FormEvent, useState } from 'react'
 import { CardCountry } from '../components/CardCountry'
+import Papa from 'papaparse'
 
 export function Home() {
   const { countries, loading } = useCountry()
   const [search, setSearch] = useState('')
-  const [viewType, setViewType] = useState<'table' | 'card'>('table')
+  const [historySearch, setHistorySearch] = useState<string[]>(() => {
+    const historyOnStorage = localStorage.getItem('history')
 
-  function handleSearch(data: CountriesData[]) {
-    if (search !== '') {
-      const dataFiltered = data.filter((item) =>
-        item.name.common.toLowerCase().includes(search.toLowerCase()),
-      )
-
-      return dataFiltered
+    if (historyOnStorage) {
+      return JSON.parse(historyOnStorage)
     }
 
-    return data
+    return []
+  })
+
+  const [viewType, setViewType] = useState<string>('table')
+
+  function handleSearch(event: FormEvent) {
+    event.preventDefault()
+
+    if (search !== '') {
+      addToHistory(search)
+      executeSearch(search)
+    }
   }
 
+  function addToHistory(query: string) {
+    const history = [query, ...historySearch]
+
+    setHistorySearch(history)
+
+    localStorage.setItem('history', JSON.stringify(history))
+  }
+
+  function executeSearch(query: string) {
+    setSearch(query)
+  }
+
+  function exportToCSV(data: unknown[]) {
+    const csv = Papa.unparse(data)
+
+    const csvData = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
+    const csvURL = window.URL.createObjectURL(csvData)
+    const tempLink = document.createElement('a')
+    tempLink.href = csvURL
+    tempLink.setAttribute('download', 'data.csv')
+    tempLink.click()
+  }
   return (
     <>
       <div className="w-full">
-        {/* <img
-          className="absolute top-[20%] left-[50%] translate-x-[-50%]"
-          src={Logo}
-          alt=""
-        /> */}
         <img className="w-full relative -z-10" src={BgHero} alt="" />
       </div>
       <main className="max-w-7xl min-h-[85vh] max-h-[85vh] px-4 py-10 -mt-20 mx-auto mb-8 bg-zinc-800 rounded-lg">
         <header className="flex items-center justify-between mb-8">
           <span className="font-semibold text-zinc-400 text-xl">
-            {handleSearch(countries).length} países encontrados
+            {
+              countries.filter((item) =>
+                item.name.common.toLowerCase().includes(search.toLowerCase()),
+              ).length
+            }{' '}
+            países encontrados
           </span>
-          <Input
-            placeholder="Pesquise um país pelo nome"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
+
+          <form onSubmit={handleSearch} className="flex items-center gap-4">
+            <Input
+              placeholder="Pesquise um país pelo nome"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button type="submit" className="p-3 bg-zinc-900 rounded-lg">
+              Salvar pesquisa
+            </button>
+          </form>
         </header>
         <div className="grid grid-cols-table gap-8">
           <section className="border-r-2 border-zinc-700 pr-4">
@@ -55,25 +90,58 @@ export function Home() {
                 onChange={(e) => setViewType(e.target.value)}
               />
             </OrganizerWrapper>
+            {historySearch.length > 0 && (
+              <OrganizerWrapper title="Últimas pesquisas">
+                <ul>
+                  {historySearch.slice(0, 5).map((query, index) => (
+                    <li key={index}>
+                      <button onClick={() => executeSearch(query)}>
+                        {query}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </OrganizerWrapper>
+            )}
+            <OrganizerWrapper title="Exportar dados">
+              <button
+                className="p-3 bg-zinc-900 rounded-lg"
+                onClick={() => exportToCSV(countries)}
+              >
+                Exportar dados em CSV
+              </button>
+            </OrganizerWrapper>
           </section>
-          <section className="overflow-y-auto min-h-[65vh] max-h-[65vh]">
+          <section className="min-h-[65vh] max-h-[65vh] overflow-y-auto">
             {viewType === 'table' ? (
               <HeaderTable>
                 {loading ? (
                   <p>Carregando...</p>
                 ) : (
                   <>
-                    {handleSearch(countries).map((item) => (
-                      <ContentTable data={item} key={item.name.common} />
-                    ))}
+                    {countries
+                      .filter((item) =>
+                        item.name.common
+                          .toLowerCase()
+                          .includes(search.toLowerCase()),
+                      )
+                      .map((item) => (
+                        <ContentTable data={item} key={item.name.common} />
+                      ))}
                   </>
                 )}
               </HeaderTable>
             ) : (
               <div className="grid grid-cols-3 gap-2">
-                {handleSearch(countries).map((item) => (
-                  <CardCountry data={item} key={item.name.common} />
-                ))}
+                {countries
+                  .filter((item) =>
+                    item.name.common
+                      .toLowerCase()
+                      .includes(search.toLowerCase()),
+                  )
+                  .map((item) => (
+                    <CardCountry data={item} key={item.name.common} />
+                  ))}
               </div>
             )}
           </section>
